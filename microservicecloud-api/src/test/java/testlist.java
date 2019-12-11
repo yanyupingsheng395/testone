482151f818c3afaec9e1fa64f7a7e936deb89950
yanyupingsheng395 <54097007+yanyupingsheng395@users.noreply.github.com>
2019-12-11 15:00:27 +0800

    Update testlist.java

--- microservicecloud-api/src/test/java/testlist.java
+++ microservicecloud-api/src/test/java/testlist.java
@@ -13,9 +13,9 @@ public class testlist {
 */
     public static void main(String[] args) {
         List aaa=new ArrayList<>();
-        int c=2;
+        int dddd=0;
         for (int i = 0; i < 20; i++) {
-            c++;
+            dddd++;
             aaa.add(i,i++);
             System.out.println("ss");
         }
